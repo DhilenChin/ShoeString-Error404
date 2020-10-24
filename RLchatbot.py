339af36079907a94d8a2bd724a0579770fd5b3e5
Dhilen Chin <dhilen717@hotmail.com>
2020-10-24 20:26:40 +0100

    RL for sorting

---
+++ RLchatbot.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('milling_machine.csv')
+df['Points'] = 0
+
+
+def sorting_best_solution (input, df, machine, trouble, analysis, solution):
+    #input should only be 'yes' or 'no' , anything else shld be converted to these two
+    if input == 'yes':
+        for index, rows in df.iterrows():
+            if df.iloc[index, 0] == machine and df.iloc[index, 1] == trouble and df.iloc[index, 2] == analysis and df.iloc[index, 3] == solution:
+                df.iloc[index, 4] =+1 
+
+    
+machine = 'Milling Machine'
+trouble = 'The workpiece milled is not flat'
+analysis = 'The spindle bearing is loose'
+solution = 'Adjust the spindle bearing gap'
+
+sorting_best_solution ('yes', df, machine, trouble, analysis, solution)
+print(df)
+
