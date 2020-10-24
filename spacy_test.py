ddbac02a897c19d0633ff2e9288d10ebc2f40d1d
psosnin <philipsosnin@gmail.com>
2020-10-24 19:59:21 +0100

    spacy_test updated

--- spacy_test.py
+++ spacy_test.py
@@ -1,32 +1,24 @@
-import spacy
 import pandas as pd
+from keywords import keywords
 
-nlp = spacy.load("en")
 text = input("What machine are you having problems with?")
-doc = nlp(text.lower())
 
-keywords = []
-for token in doc:
-    #print(token.text, token.pos_, token.dep_, token.head.text)
-    if token.pos_ == 'NOUN' or token.pos_ == 'VERB' or token.pos_ == 'ADJ' or token.pos_ == "PROPN":
-        keywords.append(token.text)
+keys = keywords(text)
 
 milling = pd.read_csv('milling_machine.csv')
 edm = pd.read_csv('edm_milling.csv')
 print(milling.head())
 print(edm.head())
 
-"""
 #simple keyword test
 print(keywords)
-if 'cnc' in keywords:
+if 'cnc' in keys:
     print("You are having problems with a CNC milling machine")
-elif 'edm' in keywords:
+elif 'edm' in keys:
     print('You are having problems with an electrical discharge milling machine')
-elif 'sandvik' in keywords:
+elif 'sandvik' in keys:
     print('You are having problems with a sandvik milling machine')
-elif 'milling' in keywords:
+elif 'milling' in keys:
     print("You are having problems with a milling machine")
 else:
     print("I'm sorry, I don't know that machine")
-    """
