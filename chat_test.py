b743afb80daa9cbd4bfc385bef54bbcd75f0ea9f
psosnin <philipsosnin@gmail.com>
2020-10-24 20:53:24 +0100

    chat test example

---
+++ chat_test.py
@@ -0,0 +1,33 @@
+from pandas_to_tree import pandas_to_tree
+import pandas as pd
+from keywords import keywords
+from decision import best_branch
+
+
+milling = pandas_to_tree(pd.read_csv('milling_machine.csv'))
+edm = pandas_to_tree(pd.read_csv('edm_milling.csv'))
+
+text = input("What machine are you having problems with?")
+
+keys = keywords(text)
+
+if 'cnc' in keys:
+    print("You are having problems with a CNC milling machine")
+    print('No data available for CNC machine')
+elif 'edm' in keys:
+    print('You are having problems with an electrical discharge milling machine')
+    data = edm
+elif 'sandvik' in keys:
+    print('You are having problems with a sandvik milling machine')
+    print('No data available for sandvik machine')
+elif 'milling' in keys:
+    print("You are having problems with a milling machine")
+    data = milling
+else:
+    print("I'm sorry, I don't know that machine")
+
+text = input("What is your issue with the machine?")
+
+branch = best_branch(data, text)
+print('Your issue is', branch.label)
+print('Try', branch.branches[0].label)
--- decision.py
+++ decision.py
@@ -1,16 +1,17 @@
 from pandas_to_tree import pandas_to_tree
+from tree import Tree
 import pandas as pd
 from keywords import keywords
 
 def best_branch(tree, text_in):
     """A function that returns the branch of TREE that has the most matching keywords with TEXT_IN"""
     keys_in = keywords(text_in)
-    print(keys_in)
+    #print(keys_in)
     best_intersect = 0
-
+    best_branch = Tree('No matching issues found')
     for branch in tree.branches:
         intersect = len(list(set(keys_in).intersection(set(branch.keywords))))
-        print(branch.keywords)
+        #print(branch.keywords)
         if best_intersect < intersect:
             best_intersect = intersect
             best_branch = branch
@@ -18,11 +19,11 @@ def best_branch(tree, text_in):
     return best_branch
 
 
-'''
-dataframe = pd.read_csv('milling_machine.csv')
+"""
+dataframe = pd.read_csv('edm_milling.csv')
 
 tree = pandas_to_tree(dataframe)
 
 text_in = input('Keywords')
 print(best_branch(tree, text_in).label)
-'''
+"""
