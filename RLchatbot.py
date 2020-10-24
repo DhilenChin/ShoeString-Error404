c47d9424ccca9793ca6ff35fe569d1fe41e9b966
Dhilen Chin <dhilen717@hotmail.com>
2020-10-24 21:18:13 +0100

    RL function fo chatbot

--- RLchatbot.py
+++ RLchatbot.py
@@ -7,6 +7,7 @@ df['Points'] = 0
 
 def sorting_best_solution (input, df, machine, trouble, analysis, solution):
     #input should only be 'yes' or 'no' , anything else shld be converted to these two
+    # df is dataframe and everything else is a string
     if input == 'yes':
         for index, rows in df.iterrows():
             if df.iloc[index, 0] == machine and df.iloc[index, 1] == trouble and df.iloc[index, 2] == analysis and df.iloc[index, 3] == solution:
