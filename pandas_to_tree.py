38864db06cf533d145b7a5b3f8efad2cf4abd9d9
psosnin <philipsosnin@gmail.com>
2020-10-24 19:43:41 +0100

    tree progress

--- pandas_to_tree.py
+++ pandas_to_tree.py
@@ -5,17 +5,18 @@ from tree import Tree
 def pandas_to_tree(dataframe):
     tree = Tree('Milling machine')
     troubles = dataframe['Trouble'].unique()
-    for trouble in troubles:
-        #analysis = dataframe[trouble]
-        #print(analysis)
-        tree.branches.append(Tree(trouble))
-        
+    for i in range(len(troubles)):
+        analysis = dataframe.loc[dataframe['Trouble'] == troubles[i]]['Analysis'].values
+        tree.branches.append(Tree(troubles[i]))
+        for j in range(len(analysis)):
+            solutions = dataframe.loc[dataframe['Analysis'] == analysis[j]]['Solution'].values
+            tree.branches[i].branches.append(Tree(analysis[j]))
     return tree
 
 
 
-dataframe = pd.read_csv('edm_milling.csv')
-print(dataframe.head())
-#tree = pandas_to_tree(dataframe)
-print(dataframe)
+dataframe = pd.read_csv('milling_machine.csv')
+#print(dataframe.head())
+tree = pandas_to_tree(dataframe)
+#print(dataframe.loc[dataframe['Trouble'] == 'No power Input'])
 #print(tree)
