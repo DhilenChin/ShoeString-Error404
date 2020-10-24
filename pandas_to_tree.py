14002f1090aa7d349a22cdabd315e222f7dd2bfc
psosnin <philipsosnin@gmail.com>
2020-10-24 19:50:16 +0100

    pandas_to_tree function done

--- pandas_to_tree.py
+++ pandas_to_tree.py
@@ -10,13 +10,12 @@ def pandas_to_tree(dataframe):
         tree.branches.append(Tree(troubles[i]))
         for j in range(len(analysis)):
             solutions = dataframe.loc[dataframe['Analysis'] == analysis[j]]['Solution'].values
-            tree.branches[i].branches.append(Tree(analysis[j]))
+            tree.branches[i].branches.append(Tree(analysis[j], branches = [Tree(sol) for sol in solutions]))
     return tree
 
 
 
 dataframe = pd.read_csv('milling_machine.csv')
-#print(dataframe.head())
+
 tree = pandas_to_tree(dataframe)
-#print(dataframe.loc[dataframe['Trouble'] == 'No power Input'])
-#print(tree)
+print(tree)
