ecdcb0f0080a779befadddb87579293e339a7915
ltm34 <ltm34@cam.ac.uk>
2020-10-24 20:24:24 +0100

    decision logic

---
+++ flow.py
@@ -0,0 +1,15 @@
+def decision_logic(big_tree):
+    branch = high_score(big_tree)
+    return branch.lable
+
+def high_score(branch):
+    children = branch.branches
+    max_element = 0
+    for i in range(len(children)):
+        if children[i].score > children[max_element].score and not children[i].used:
+            max_element = i
+    return children[max_element]
+
+
+
+
