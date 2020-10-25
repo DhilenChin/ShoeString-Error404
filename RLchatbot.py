85c311e90177934878045014dd47f6d30483c117
Dhilen Chin <dhilen717@hotmail.com>
2020-10-25 09:38:31 +0000

    Updated RL sorting

--- RLchatbot.py
+++ RLchatbot.py
@@ -5,20 +5,20 @@ df = pd.read_csv('milling_machine.csv')
 df['Points'] = 0
 
 
-def sorting_best_solution (input, df, machine, trouble, analysis, solution):
+def giving_points (response, df, machine, trouble, analysis, solution):
     #input should only be 'yes' or 'no' , anything else shld be converted to these two
     # df is dataframe and everything else is a string
-    if input == 'yes':
+    if response == 'yes':
         for index, rows in df.iterrows():
             if df.iloc[index, 0] == machine and df.iloc[index, 1] == trouble and df.iloc[index, 2] == analysis and df.iloc[index, 3] == solution:
                 df.iloc[index, 4] =+1 
 
-    
-machine = 'Milling Machine'
-trouble = 'The workpiece milled is not flat'
-analysis = 'The spindle bearing is loose'
-solution = 'Adjust the spindle bearing gap'
+    df.sort_values(['Trouble', 'Points'], ascending = [1,0])
+    return None 
 
-sorting_best_solution ('yes', df, machine, trouble, analysis, solution)
-print(df)
+
+
+
+
+
 
